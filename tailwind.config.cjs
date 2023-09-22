11d0189c90c3d3386c68987999645aa1019ea25b
Victor Hugo Molinar <vhmolinar@gmail.com>
2023-09-21 22:29:32 -0300

    feat: meta tags e cores de texto

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,8 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		colors: {
-			primary: "#a1a5c0"
+			primary: "#a1a5c0",
+			sweetblack: "#2f2e2e"
 		},
 	},
 	plugins: [],
