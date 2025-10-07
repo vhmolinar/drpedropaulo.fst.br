1d59e2e653ed2e85a3d208454a957d53d46a5a70
Victor Hugo Molinar <vhmolinar@gmail.com>
2025-10-07 01:09:38 -0300

    feat: redesign v2

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,9 +2,12 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		colors: {
-			primary: "#a1a5c0",
-			sweetblack: "#2f2e2e"
+		extend: {
+			colors: {
+				primary: "#aeded0",
+				black: "#000000",
+				white: "#ffffff",
+			},
 		},
 	},
 	plugins: [],
